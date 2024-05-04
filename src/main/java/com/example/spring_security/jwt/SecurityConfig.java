e128b1d0c87bd269b3eebb368817d75ac1fddb5b
Balloon208 <gnswldms2@gmail.com>
2024-05-05 01:25:03 +0900

    Add : SecurityConfig

--- src/main/java/com/example/spring_security/jwt/SecurityConfig.java
+++ src/main/java/com/example/spring_security/jwt/SecurityConfig.java
@@ -1,7 +1,67 @@
 package com.example.spring_security.jwt;
 
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@Configuration
+@EnableWebSecurity
+@AllArgsConstructor
 public class SecurityConfig {
+    private final JwtTokenProvider jwtTokenProvider;
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+        /*
+        return httpSecurity
+                // basic auth 사용하지 않음
+                .httpBasic().disable()
+                // csrf 보안 사용하지 않음
+                .csrf().disable()
+                // JWT를 사용하기 때문에 세션을 사용하지 않음
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeHttpRequests()
+                // 해당 API에 대해서는 모든 요청을 허가
+                .requestMatchers("/members/sign-in").permitAll()
+                // USER 권한이 있어야 요청할 수 있음
+                .requestMatchers("/members/test").hasRole("USER")
+                // 이 밖에 모든 요청에 대해서 인증을 필요로 한다는 설정
+                .anyRequest().authentic ated()
+                .and()
+                // JWT 인증을 위하여 직접 구현한 필터를 UsernamePasswordAuthenticationFilter 전에 실행
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class).build();
+        */
 
+        return httpSecurity
+                .httpBasic((httpBasicConfig)->
+                            httpBasicConfig.disable()
+                        )
+                .csrf((csrfConfig)->
+                        csrfConfig.disable()
+                )
+                .sessionManagement((sessionManagementConfig)->
+                        sessionManagementConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests((authorizeHttpRequestConfig)->
+                        authorizeHttpRequestConfig
+                                .requestMatchers("/members/sign-in").permitAll()
+                                .requestMatchers("/members/test").hasRole("USER")
+                                .anyRequest().authenticated()
+                )
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class).build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        // BCrypt Encoder 사용
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 }
